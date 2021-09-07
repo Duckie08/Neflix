3e6cd9ff6162f65a9b166407df62cca58a544b8a
Duckie08 <52843939+Duckie08@users.noreply.github.com>
2021-09-08 00:33:44 +0700

    create profiles components

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as OtpForm } from './otp-form';
 export { default as Header } from './header';
 export { default as Feature } from './feature';
 export { default as Form } from './form';
+export { default as Profiles } from './profiles';
