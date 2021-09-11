3120a947b0e6b1e5686ce2ab68b7a90ceeb19d20
Duckie08 <52843939+Duckie08@users.noreply.github.com>
2021-09-11 21:53:16 +0700

    update profile and brwose component

--- src/container/browse.js
+++ src/container/browse.js
@@ -1,20 +1,63 @@
 import React, { useContext, useState, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
+import { Header, Loading } from "../components";
+import * as ROUTES from '../constants/router';
+import logo from '../logo.svg';
 
 export function BrowseContainer({ slides }) {
+    const [searchTerm, setSearchTerm] = useState('');
     const [profile, setProfile] = useState({});
     const [loading, setLoading] = useState(true);
     const { firebase } = useContext(FirebaseContext);
+    
     const user = firebase.auth().currentUser || {};
-    console.log(user)
 
     useEffect(() => {
-        console.log(profile)
+        console.log("profile", profile)
         setTimeout(() => {
-            setLoading(false);
+          setLoading(false);
         }, 3000);
-    }, [profile.diplayName]);
+      }, [profile.displayName]);
+    
 
-    return <SelectProfileContainer user={user} setProfile={setProfile} />
+    
+    return profile.displayName ? (
+        <>
+        {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
+        <Header src="joker1" dontShowOnSmallViewPort>
+            <Header.Frame>
+                <Header.Group>
+                    <Header.Logo to={ROUTES.HOME} src={logo} alt="NetFlix" />
+                    <Header.TextLink>Series</Header.TextLink>
+                    <Header.TextLink>Films</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                    <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                </Header.Group>
+                <Header.Profile>
+                    <Header.Picture src={user.photoURL}></Header.Picture>
+                    <Header.Dropdown>
+                        <Header.Group>
+                        <Header.Picture src={user.photoURL} />
+                        <Header.TextLink>{user.displayName}</Header.TextLink>
+                        </Header.Group>
+                        <Header.Group>
+                            <Header.TextLink onClick={() => firebase.auth().signOut()}>Sign out</Header.TextLink>
+                        </Header.Group>
+                    </Header.Dropdown>
+                </Header.Profile>
+            </Header.Frame>
+            <Header.Feature>
+                <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+                <Header.Text>
+                    Forever alone in a crowd, failded comedian Athur Fleck seeks connection as he walks the streets of Gotham City. Athur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a futile attemp to feel like he's part of the world around him.
+                </Header.Text>
+            </Header.Feature>
+        </Header>
+        </>
+    ) : (
+        <SelectProfileContainer user={user} setProfile={setProfile} />
+        );
 }
--- src/container/profiles.js
+++ src/container/profiles.js
@@ -1,33 +1,35 @@
-import React from "react";
-import { Header } from "../components";
+import React from 'react';
+import { Header, Profiles } from '../components';
 import * as ROUTES from '../constants/router';
 import logo from '../logo.svg';
-import { Profiles } from "../components";
+
 
 export function SelectProfileContainer({ user, setProfile }) {
-    console.log(user)
-    return (
-        <>
-            <Header bg={false}>
-                <Header.Frame>
-                    <Header.Logo to={ROUTES.HOME} src={logo} alt="NetFlix" />
-                </Header.Frame>
-            </Header>
+  return (
+    <>
+      <Header bg={false}>
+        <Header.Frame>
+          <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+        </Header.Frame>
+      </Header>
 
-            <Profiles>
-                <Profiles.Title>Who's watching </Profiles.Title>
-                <Profiles.List>
-                    <Profiles.User onClick={() => 
-                        setProfile({
-                            displayName: user.displayName,
-                            photoURL: user.photoURL,
-                        })
-                    }>
-                        <Profiles.Picture src={user.photoURL} />
-                        <Profiles.Name>{user.displayName}</Profiles.Name>
-                    </Profiles.User>
-                </Profiles.List>
-            </Profiles>
-        </>
-    );
+      <Profiles>
+        <Profiles.Title>Who's watching?</Profiles.Title>
+        <Profiles.List>
+        <Profiles.User
+            onClick={() => 
+              setProfile({ 
+                displayName: user.displayName, 
+                photoURL: user.photoURL ,
+            })}
+            data-testid="user-profile"
+          >
+            <Profiles.Picture src={user.photoURL} />
+            <Profiles.Name>{user.displayName}</Profiles.Name>
+          </Profiles.User>
+        </Profiles.List>
+      </Profiles>
+    </>
+  );
 }
+
